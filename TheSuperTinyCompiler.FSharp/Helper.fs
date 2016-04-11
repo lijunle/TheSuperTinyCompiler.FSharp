278b5349cbf9b5439b11c58f76275fa9d595a4a4
Junle Li <lijunle@gmail.com>
2016-04-12 04:03:58 +0800

    Re-write the parser from single character parsing.
    - Reference this tutorial: https://fsharpforfunandprofit.com/posts/understanding-parser-combinators/

---
+++ TheSuperTinyCompiler.FSharp/Helper.fs
@@ -0,0 +1,29 @@
+[<AutoOpen>]
+module Helper
+
+[<AutoOpen>]
+module DomainTypes =
+    type Input = {
+        Value: string
+        Index: int
+    }
+
+    type Result<'a> =
+    | Success of 'a
+    | Failure of string
+
+module Input =
+    let init s = { Value = s; Index = 0 }
+    let next input = { input with Index = input.Index + 1 }
+    let first input = input.Value.[input.Index]
+    let empty input = input.Index = input.Value.Length
+
+let parseChar c input =
+    if Input.empty input then
+        Failure "No more input"
+    elif c = Input.first input then
+        let next = Input.next input
+        Success (c, next)
+    else
+        let message = sprintf "Expect %c, actual %c" c (Input.first input)
+        Failure message
