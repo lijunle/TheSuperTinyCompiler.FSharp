5128a97036698408c62d8215631b3d4b99e5d7be
Junle Li <lijunle@gmail.com>
2016-04-20 01:44:03 +0800

    Add parser type, return parser from parserChar function.

--- TheSuperTinyCompiler.FSharp/Helper.fs
+++ TheSuperTinyCompiler.FSharp/Helper.fs
@@ -12,18 +12,22 @@ module DomainTypes =
     | Success of 'a
     | Failure of string
 
+    type Parser<'a> = Parser of (Input -> Result<'a * Input>)
+
 module Input =
     let init s = { Value = s; Index = 0 }
     let next input = { input with Index = input.Index + 1 }
     let first input = input.Value.[input.Index]
     let empty input = input.Index = input.Value.Length
 
-let parseChar c input =
-    if Input.empty input then
-        Failure "No more input"
-    elif c = Input.first input then
-        let next = Input.next input
-        Success (c, next)
-    else
-        let message = sprintf "Expect %c, actual %c" c (Input.first input)
-        Failure message
+let parseChar c =
+    let fn input =
+        if Input.empty input then
+            Failure "No more input"
+        elif c = Input.first input then
+            let next = Input.next input
+            Success (c, next)
+        else
+            let message = sprintf "Expect %c, actual %c" c (Input.first input)
+            Failure message
+    Parser fn
