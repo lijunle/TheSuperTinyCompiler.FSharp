840ea84641ad95188ff9825e5b857c36166311ff
Junle Li <lijunle@gmail.com>
2016-04-24 00:35:55 +0800

    Refactor the functions in test helper to shorter names.

--- TheSuperTinyCompiler.FSharp/Helper.fs
+++ TheSuperTinyCompiler.FSharp/Helper.fs
@@ -5,29 +5,29 @@ let (|>>) = Parser2.(|>>)
 let (>>.) = Parser2.(>>.)
 let (.>>) = Parser2.(.>>)
 
-let parseChar c =
+let char c =
     let equal first =
         c = first
     let message first =
         sprintf "Expect %c, actual %c" c first
     Parser2.satisfy equal message
 
-let parseString (s : string) =
+let anyOf charList =
+    charList
+    |> List.map char
+    |> List.reduce Parser2.orElse
+
+let string (s : string) =
     let charListToString chars =
         System.String(List.toArray chars)
 
     s
     |> List.ofSeq
-    |> List.map parseChar
+    |> List.map char
     |> Parser2.sequenceList
     |>> charListToString
 
-let anyOf charList =
-    charList
-    |> List.map parseChar
-    |> List.reduce Parser2.orElse
-
-let parseInteger =
+let integer =
     let firstDigit = anyOf ['1'..'9']
     let digits = anyOf ['0'..'9']
     let digitsToInteger digits =
@@ -38,21 +38,25 @@ let parseInteger =
     |>> List.Cons
     |>> digitsToInteger
 
-let parseSpace =
+let space =
     let message first =
         sprintf "Expect space, actual %c" first
     Parser2.satisfy System.Char.IsWhiteSpace message
 
-let parseSpaces =
-    Parser2.many parseSpace
+let spaces =
+    Parser2.many space
+
+let leftParen = char '('
+
+let rightParen = char ')'
+
+let integerPair = integer .>> spaces .>>. integer
+
+let wrappedPair = Parser2.between leftParen integerPair rightParen
 
 let test s =
     let input = Input.init s
-    let leftParen = parseChar '('
-    let rightParen = parseChar ')'
-    let integerPair = parseInteger .>> parseSpaces .>>. parseInteger
-    let parser = Parser2.between leftParen integerPair rightParen
-    let result = Parser2.run parser input
+    let result = Parser2.run wrappedPair input
     match result with
     | Failure e -> printfn "%s" e
     | Success (v, next) -> printfn "Got %A, next index %A" v next.Index
